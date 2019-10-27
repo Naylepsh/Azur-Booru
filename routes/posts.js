9580c303bf45fc2a3db391904329ed99f7b608b0
Naylepsh <naylepsh@gmail.com>
2019-10-27 14:41:07 +0100

    refactor posts get and post routes

--- routes/posts.js
+++ routes/posts.js
@@ -4,42 +4,54 @@ const express = require('express'),
   Storage = require('../utils').Storage,
   Image = require('../models/image');
 
-router.get('/', (req, res) => {
-  // find 30 posts and print them
+const IMAGES_PER_PAGE = 20;
 
-  // working image test
-  Image.find({}, (err, binaryImages) => {
-    if (err) {
-      console.log(err);
-    } else {
-      images = [];
-      for (const image of binaryImages){
-        const base64Flag = 'data:' + image.contentType + ';base64,';
-        const imageString = image.data.toString('base64');
-        images.push(base64Flag + imageString);
-      }
-      res.render('posts/index', {images: images});
-    }
-  });
+router.get('/', async (req, res) => {
+  try {
+    // get the number of records
+    const count = await Image.countDocuments({});
+
+    // set the page info
+    const n = Math.floor(count / IMAGES_PER_PAGE);
+    const current_page = 1;
+
+    // get n-th 30 images
+    const binaryImages = await Image
+    .find({})
+    .limit(IMAGES_PER_PAGE);
+    const images = binaryImages.map( image => {
+      const base64Flag = 'data:' + image.contentType + ';base64,';
+      const imageString = image.data.toString('base64');
+      return base64Flag + imageString;
+    });
+
+    res.render('posts/index', {
+      images: images, 
+      page_info: {
+        n: n, 
+        current_page: current_page
+      }});
+
+  } catch(err) {
+    console.log(err);
+  }
 });
 
 router.get('/new', (req, res) => {
   res.render('posts/new');
 });
 
-router.post('/', Storage.single('image'), (req, res) => {
-  Image.create({
+router.post('/', Storage.single('image'), async (req, res) => {
+  try {
+    const image = await Image.create({
     data: fs.readFileSync(req.file.path),
-    contentType: req.file.mimetype
-  }), (err, img) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(img);
-    }
-  };
-  fs.unlinkSync(req.file.path);
-  res.redirect('/posts');
+    contentType: req.file.mimetype});
+    fs.unlinkSync(req.file.path);
+    res.redirect('/posts');
+  } catch (err){
+    console.log(err);
+    res.redirect('/get');
+  }
 });
 
 module.exports = router;
