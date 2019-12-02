d556855107b5180e5307301918569e7f52bfe994
Naylepsh <naylepsh@gmail.com>
2019-12-02 16:40:58 +0100

    changes to mobile menu color on different pages

--- public/js/nav-bar.js
+++ public/js/nav-bar.js
@@ -1,9 +1,11 @@
 /* scripts for handling navbar related operations */
 
 const mobileMenu = document.getElementsByClassName('mobile-menu')[0];
+const navBar = document.querySelector('nav');
 
 function toggleMobileMenu(data) {
   mobileMenu.classList.toggle('disabled');
+  navBar.classList.toggle('extending-element');
 }
 
 const toggler = document.getElementById('mobile-menu-toggler');
