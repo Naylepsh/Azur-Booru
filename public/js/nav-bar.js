afc28490c1688a19e81bfb71fdfb4614092ef039
Naylepsh <naylepsh@gmail.com>
2020-01-01 16:32:53 +0100

    more navbar customizations

--- public/js/nav-bar.js
+++ public/js/nav-bar.js
@@ -5,7 +5,7 @@
   let $ = el => document.querySelector(el);
   
   $(".animenu__btn").addEventListener("click", function(){
-    this.classList.toggle("animenu__btn--active")
-    $(".animenu__nav").classList.toggle("animenu__nav--active")
+    this.classList.toggle("animenu__btn--active");
+    $(".animenu__nav").classList.toggle("animenu__nav--active");
   });
 })()
