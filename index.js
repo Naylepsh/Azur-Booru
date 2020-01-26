678034c0ab6a2d17b3f11c9f0d67b67e501c172f
Naylepsh <naylepsh@gmail.com>
2020-01-26 18:33:58 +0100

    add logger

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const logger = require('morgan');
 const db = require('./config').mongoose;
 
 const indexRoutes = require('./routes/index.js');
@@ -9,11 +10,13 @@ const postsRoutes = require('./routes/posts');
 const PORT = 3000;
 
 // App Config
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+app.use(logger('dev'));
 app.use(indexRoutes);
 app.use('/posts', postsRoutes);
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
+
   
 app.listen(PORT, () => {
     console.log(`Booru server started at port ${PORT}`);
