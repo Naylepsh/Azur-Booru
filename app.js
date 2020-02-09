f64dab7c57a9cfcb69d55bff09491a1da1af9ffc
Naylepsh <naylepsh@gmail.com>
2020-02-09 14:54:52 +0100

    change some code structure

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const db = require('./config').mongoose;
 const indexRoutes = require('./routes/index.js');
 const postsRoutes = require('./routes/posts');
 
-const PORT = 3000;
-
 // App Config
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
@@ -19,7 +17,4 @@ app.use(methodOverride('_method'));
 app.use(indexRoutes);
 app.use('/posts', postsRoutes);
 
-  
-app.listen(PORT, () => {
-    console.log(`Booru server started at port ${PORT}`);
-});
+module.exports = app;
