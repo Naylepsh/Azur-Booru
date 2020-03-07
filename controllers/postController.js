f97f62c10f2c27328fd414baab7248493fc451fd
Naylepsh <naylepsh@gmail.com>
2020-03-07 17:21:02 +0100

    remove leftover console.logs

--- controllers/postController.js
+++ controllers/postController.js
@@ -60,10 +60,6 @@ exports.create = async (req, res) => {
     const tags = await Tag.findOrCreateMany(req.body.post.tags.map(name => {
       return {name};
     }), session);
-    console.log('tags done');
-    for (const tag of tags) {
-      // console.log(tag);
-    }
     const tagsIds = tags.map(tag => tag._id);
 
     const post = await Post.create([{
@@ -75,9 +71,6 @@ exports.create = async (req, res) => {
       author: req.user._id,
       score: 0
     }], { session });
-    for (const tag of tags) {
-      console.log(tag);
-    }
     await Promise.all(tags.map(tag => tag.addPost(post._id)));
     await session.commitTransaction();
   } catch (error) {
