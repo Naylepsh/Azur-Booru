d07d9222c74380de5b107589de9f091ce9220e23
naylepsh <naylepsh@gmail.com>
2020-04-08 20:09:36 +0200

    use error pages in error middleware

--- middleware/error.js
+++ middleware/error.js
@@ -1,6 +1,11 @@
 const { sendError } = require("../utils/misc");
 
 module.exports = (err, req, res, next) => {
-  console.error(err);
-  sendError(res, { status: 500, message: "Something went wrong." });
+  const { status, message } = err;
+  console.log(message);
+  if (status === 404) {
+    res.redirect("/not-found");
+  } else {
+    res.redirect("/internal-error");
+  }
 };
