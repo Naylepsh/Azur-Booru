30531899267bcdc10fc8b3f9b4874c3187fea804
naylepsh <naylepsh@gmail.com>
2020-04-14 14:47:21 +0200

    make navbar responsive

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,146 +1,157 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link, NavLink } from "react-router-dom";
 
-const NavBar = () => {
-  return (
-    <nav className="animenu">
-      <Link id="animenu__brand" to="/">
-        Azur-Booru
-      </Link>
-      <button className="animenu__btn">
-        <Link id="mobile-menu-toggler">
-          <i className="fas fa-bars"></i>
+class Navbar extends Component {
+  state = {
+    isNavActive: false,
+  };
+
+  handleNavToggle = () => {
+    let { isNavActive } = this.state;
+    isNavActive = !isNavActive;
+    this.setState({ isNavActive });
+  };
+
+  getNavClass = () => {
+    const { isNavActive } = this.state;
+    return isNavActive ? "animenu__nav animenu__nav--active" : "animenu__nav";
+  };
+
+  render() {
+    return (
+      <nav className="animenu">
+        <Link id="animenu__brand" to="/">
+          Azur-Booru
         </Link>
-      </button>
-
-      <ul className="animenu__nav">
-        <li>
-          <Link
-            to="/posts"
-            className="animenu__nav__hasDropdown"
-            aria-haspopup="true"
-          >
-            Posts
+        <button className="animenu__btn" onClick={this.handleNavToggle}>
+          <Link id="mobile-menu-toggler">
+            <i class="fa fa-bars" aria-hidden="true"></i>
           </Link>
-          <ul
-            className="animenu__nav__dropdown"
-            aria-label="submenu"
-            role="menu"
-          >
-            <li>
-              <NavLink to="/posts" role="menuitem">
-                List
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/posts/new" role="menuitem">
-                Upload
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/" role="menuitem">
-                Hot
-              </NavLink>
-            </li>
-          </ul>
-        </li>
+        </button>
 
-        <li>
-          <Link
-            to="/comments"
-            className="animenu__nav__hasDropdown"
-            aria-haspopup="true"
-          >
-            Comments
-          </Link>
-          <ul
-            className="animenu__nav__dropdown"
-            aria-label="submenu"
-            role="menu"
-          >
-            <li>
-              <NavLink to="/comments" role="menuitem">
-                List
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/comments/search" role="menuitem">
-                Search
-              </NavLink>
-            </li>
-          </ul>
-        </li>
+        <ul className={this.getNavClass()}>
+          <li>
+            <Link
+              to="/posts"
+              className="animenu__nav__hasDropdown"
+              aria-haspopup="true"
+            >
+              Posts
+            </Link>
+            <ul
+              className="animenu__nav__dropdown"
+              aria-label="submenu"
+              role="menu"
+            >
+              <li>
+                <NavLink to="/posts" role="menuitem">
+                  List
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/posts/new" role="menuitem">
+                  Upload
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/" role="menuitem">
+                  Hot
+                </NavLink>
+              </li>
+            </ul>
+          </li>
 
-        <li>
-          <Link
-            to="#"
-            className="animenu__nav__hasDropdown"
-            aria-haspopup="true"
-          >
-            Tags
-          </Link>
-          <ul
-            className="animenu__nav__dropdown"
-            aria-label="submenu"
-            role="menu"
-          >
-            <li>
-              <NavLink to="#" role="menuitem">
-                List
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="#" role="menuitem">
-                Search
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="#" role="menuitem">
-                Aliases
-              </NavLink>
-            </li>
-          </ul>
-        </li>
+          <li>
+            <Link
+              to="/comments"
+              className="animenu__nav__hasDropdown"
+              aria-haspopup="true"
+            >
+              Comments
+            </Link>
+            <ul
+              className="animenu__nav__dropdown"
+              aria-label="submenu"
+              role="menu"
+            >
+              <li>
+                <NavLink to="/comments" role="menuitem">
+                  List
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/comments/search" role="menuitem">
+                  Search
+                </NavLink>
+              </li>
+            </ul>
+          </li>
 
-        <li>
-          <Link
-            href="#"
-            className="animenu__nav__hasDropdown"
-            aria-haspopup="true"
-          >
-            My Account
-          </Link>
-          <ul
-            className="animenu__nav__dropdown"
-            aria-label="submenu"
-            role="menu"
-          >
-            <li>
-              <NavLink to="/logout" role="menuitem">
-                Logout
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/login" role="menuitem">
-                Login
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/register" role="menuitem">
-                Sign Up
-              </NavLink>
-            </li>
-          </ul>
-        </li>
-      </ul>
+          <li>
+            <Link
+              to="#"
+              className="animenu__nav__hasDropdown"
+              aria-haspopup="true"
+            >
+              Tags
+            </Link>
+            <ul
+              className="animenu__nav__dropdown"
+              aria-label="submenu"
+              role="menu"
+            >
+              <li>
+                <NavLink to="#" role="menuitem">
+                  List
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="#" role="menuitem">
+                  Search
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="#" role="menuitem">
+                  Aliases
+                </NavLink>
+              </li>
+            </ul>
+          </li>
 
-      {/* <ul className="animenu__nav">
+          <li>
+            <Link
+              href="#"
+              className="animenu__nav__hasDropdown"
+              aria-haspopup="true"
+            >
+              My Account
+            </Link>
+            <ul
+              className="animenu__nav__dropdown"
+              aria-label="submenu"
+              role="menu"
+            >
+              <li>
+                <NavLink to="/logout" role="menuitem">
+                  Logout
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/login" role="menuitem">
+                  Login
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/register" role="menuitem">
+                  Sign Up
+                </NavLink>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
 
-        
-        
-      </ul> */}
-    </nav>
-  );
-};
-
-export default NavBar;
+export default Navbar;
