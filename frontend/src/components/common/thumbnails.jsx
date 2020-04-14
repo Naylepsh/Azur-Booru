6ce9a584b014dd4a5462bc910a2529bc73c13cb1
naylepsh <naylepsh@gmail.com>
2020-04-14 17:02:58 +0200

    add thumbnails

---
+++ frontend/src/components/common/thumbnails.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const getLinkToFullsize = (url, id) => {
+  return `${url}/${id}`;
+};
+
+const Thumbnails = ({ thumbnails, url }) => {
+  return (
+    <section className="thumbnails">
+      {thumbnails.map((thumbnail) => (
+        <div className="thumbnail-preview">
+          <Link to={getLinkToFullsize(url, thumbnail.id)}>
+            <img src={thumbnail.source} alt={thumbnail.id} />
+          </Link>
+        </div>
+      ))}
+    </section>
+  );
+};
+
+export default Thumbnails;
--- frontend/src/components/posts.jsx
+++ frontend/src/components/posts.jsx
@@ -2,10 +2,24 @@ import React, { Component } from "react";
 import "../css/posts.css";
 import SearchBar from "./searchBar";
 import PostsSidebar from "./postsSidebar";
+import Thumbnails from "./common/thumbnails";
 
 class Posts extends Component {
   state = {
     tags: [{ name: "tag1", occurences: 5 }],
+    thumbnails: [
+      { id: 1, source: "https://picsum.photos/200/200" },
+      { id: 2, source: "https://picsum.photos/200/200" },
+      { id: 3, source: "https://picsum.photos/200/200" },
+      { id: 4, source: "https://picsum.photos/200/200" },
+      { id: 5, source: "https://picsum.photos/200/200" },
+      { id: 10, source: "https://picsum.photos/200/200" },
+      { id: 20, source: "https://picsum.photos/200/200" },
+      { id: 30, source: "https://picsum.photos/200/200" },
+      { id: 40, source: "https://picsum.photos/200/200" },
+      { id: 50, source: "https://picsum.photos/200/200" },
+    ],
+    url: "/",
   };
 
   render() {
@@ -13,6 +27,9 @@ class Posts extends Component {
       <div class="container text-center">
         <SearchBar />
         <PostsSidebar tags={this.state.tags} />
+        <div id="content">
+          <Thumbnails thumbnails={this.state.thumbnails} url={this.state.url} />
+        </div>
       </div>
     );
   }
