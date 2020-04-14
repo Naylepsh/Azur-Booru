cc554e956b5c941a9c1ba7a71f3de4a2d1386d2d
naylepsh <naylepsh@gmail.com>
2020-04-14 20:49:35 +0200

    fix posts page

--- frontend/src/components/postsSidebar.jsx
+++ frontend/src/components/postsSidebar.jsx
@@ -1,22 +1,14 @@
 import React, { Component } from "react";
 import TagsSidebarSection from "./tagsSidebarSection";
-import PostInfoSidebarSection from "./postInfoSidebarSection";
 
 class PostsSidebar extends Component {
   state = {};
   render() {
-    const { tags, post } = this.props;
-    const { id, source, score, rating } = post;
+    const { tags } = this.props;
 
     return (
       <aside className="sidebar">
         <TagsSidebarSection tags={tags} />
-        <PostInfoSidebarSection
-          id={id}
-          source={source}
-          score={score}
-          rating={rating}
-        />
       </aside>
     );
   }
