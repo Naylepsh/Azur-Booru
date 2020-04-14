60915bae6b2f710634ede4db920ee4e633d8b8fd
naylepsh <naylepsh@gmail.com>
2020-04-14 19:41:37 +0200

    show post image

--- frontend/src/components/post.jsx
+++ frontend/src/components/post.jsx
@@ -17,12 +17,32 @@ class Post extends Component {
     this.setState({ post: data.post, tags: data.tags });
   }
 
+  renderPostContent() {
+    return (
+      <section className="post-image">
+        <img src={this.state.post.imageLink} />
+        <menu className="post-menu">
+          <li>
+            <button id="post-vote-up">Vote up</button>
+          </li>
+          <li>
+            <button id="post-vote-down">Vote down</button>
+          </li>
+          <li>
+            <button id="post-favorite">Favorite</button>
+          </li>
+        </menu>
+      </section>
+    );
+  }
+
   render() {
     const { post, tags } = this.state;
     return (
       <div className="container">
         <SearchBar />
         <PostSidebar tags={tags} post={post} />
+        <div id="content">{this.renderPostContent()}</div>
       </div>
     );
   }
