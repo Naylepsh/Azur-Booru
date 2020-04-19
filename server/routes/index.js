215435b02e7d5c1a933875bc4a847a4e4ca7140c
naylepsh <naylepsh@gmail.com>
2020-04-19 20:01:45 +0200

    rename folder

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(require("./error"));
 
   // load apis
-  app.use("/api/v1/posts", require("./v1.0/posts"));
-  // app.use("/api/v1.0/", require("./v1.0/user"));
-  app.use("/api/v1/comments", require("./v1.0/comments"));
+  app.use("/api/v1/posts", require("./v1/posts"));
+  // app.use("/api/v1/", require("./v1/user"));
+  app.use("/api/v1/comments", require("./v1/comments"));
 };
