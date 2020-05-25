db6c02d94564b86f930a5b80d9583b4a2a4a6440
Naylepsh <naylepsh@gmail.com>
2020-05-25 15:36:13 +0200

    show comment preview (post thumbnail + some comment info)

--- client/src/components/Comments/comments.jsx
+++ client/src/components/Comments/comments.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Comment from "./../common/Comments/comment";
 import { getComments } from "./../../services/commentService";
 
 class Comments extends Component {
@@ -13,14 +14,31 @@ class Comments extends Component {
     this.setState({ comments });
   }
 
+  commentPreview = (comment) => {
+    return (
+      <div className="comment-preview">
+        <div className="thumbnail">
+          <a href={`/posts/${comment.post.id}`}>
+            <img src={comment.post.thumbnailLink}></img>
+          </a>
+        </div>
+        <Comment {...comment} />
+      </div>
+    );
+  };
+
   render() {
     const { comments } = this.state;
 
     return (
-      <div>
-        {comments.map((comment) => (
-          <p>{comment.body}</p>
-        ))}
+      <div className="container">
+        <section className="comments">
+          {comments && comments.length > 0 && (
+            <div className="commentsList">
+              <ul>{comments.map(this.commentPreview)}</ul>
+            </div>
+          )}
+        </section>
       </div>
     );
   }
