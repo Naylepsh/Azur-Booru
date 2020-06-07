cc986c537e1f509192489ef30bcb9f598b4d7307
Naylepsh <naylepsh@gmail.com>
2020-06-07 13:02:13 +0200

    remove dead routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,4 @@
 module.exports = function (app) {
-  // app.use(express.json());
-  app.use(require("./landing"));
-  app.use(require("./error"));
-
   // load apis
   app.use("/api/v1/posts", require("./v1/posts"));
   app.use("/api/v1/users", require("./v1/user"));
