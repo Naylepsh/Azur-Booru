bb685d9aa229f9a4f0d39cda30f610bbe7ae1bab
Naylepsh <naylepsh@gmail.com>
2020-06-07 12:56:29 +0200

    fix error wrapper not passing status codes

--- server/middleware/error.js
+++ server/middleware/error.js
@@ -1,10 +1,5 @@
 module.exports = (err, req, res, next) => {
   const { status, message } = err;
   console.log(err);
-  res.send(message);
-  // if (status === 404) {
-  //   res.redirect("/not_found");
-  // } else {
-  //   res.redirect("/internal_error");
-  // }
+  res.status(status).send(message);
 };
