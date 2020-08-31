7e9f8567377d45611d89e447a4397520630c8136
Naylepsh <naylepsh@gmail.com>
2020-08-31 14:35:02 +0200

    change file name

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,6 @@
 module.exports = function (app) {
   app.use("/api/v1/posts", require("./v1/posts"));
   app.use("/api/v1/users", require("./v1/user.routing"));
-  app.use("/api/v1/comments", require("./v1/comments"));
+  app.use("/api/v1/comments", require("./v1/comment.routing"));
   app.use("/api/v1/images", require("./v1/images"));
 };
